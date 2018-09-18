2109492e6d2809b7052504098020a9809c3c38a7
Dayane Peres <dayane.fernandaperes@gmail.com>
2018-09-18 17:43:30 -0300

    Tela Vender Produto

---
+++ HAIR LUMOS/HairLumos-2.0/HairLumos/Views/Funcoes Fundamentais/RF_F7  Vender Produto/VenderProduto.Designer.cs
@@ -0,0 +1,408 @@
+﻿namespace HairLumos.Views.Funcoes_Fundamentais.RF_F7_Vender_Produto
+{
+    partial class VenderProduto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.btnExcluirItem = new System.Windows.Forms.Button();
+            this.dgvProdutos = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Prod = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Qtde = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnPesquisarProduto = new System.Windows.Forms.Button();
+            this.ttbProduto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.mskTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.Código = new System.Windows.Forms.Label();
+            this.btnPesquisarCliente = new System.Windows.Forms.Button();
+            this.ttbCliente = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnFecharVenda = new System.Windows.Forms.Button();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnNovo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BackColor = System.Drawing.Color.White;
+            this.splitContainer1.Location = new System.Drawing.Point(12, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.btnFecharVenda);
+            this.splitContainer1.Panel2.Controls.Add(this.btnPesquisar);
+            this.splitContainer1.Panel2.Controls.Add(this.btnCancelar);
+            this.splitContainer1.Panel2.Controls.Add(this.btnSair);
+            this.splitContainer1.Panel2.Controls.Add(this.btnExcluir);
+            this.splitContainer1.Panel2.Controls.Add(this.btnAlterar);
+            this.splitContainer1.Panel2.Controls.Add(this.btnNovo);
+            this.splitContainer1.Size = new System.Drawing.Size(776, 479);
+            this.splitContainer1.SplitterDistance = 420;
+            this.splitContainer1.SplitterWidth = 1;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.groupBox4);
+            this.groupBox2.Controls.Add(this.groupBox3);
+            this.groupBox2.Location = new System.Drawing.Point(21, 107);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(742, 310);
+            this.groupBox2.TabIndex = 18;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Produto";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.groupBox5);
+            this.groupBox4.Controls.Add(this.btnExcluirItem);
+            this.groupBox4.Controls.Add(this.dgvProdutos);
+            this.groupBox4.Location = new System.Drawing.Point(9, 122);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(717, 182);
+            this.groupBox4.TabIndex = 2;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Lista de Produto Selecionado";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.maskedTextBox2);
+            this.groupBox5.Location = new System.Drawing.Point(595, 84);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(116, 83);
+            this.groupBox5.TabIndex = 27;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Valor (R$)";
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(6, 40);
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(104, 20);
+            this.maskedTextBox2.TabIndex = 24;
+            // 
+            // btnExcluirItem
+            // 
+            this.btnExcluirItem.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnExcluirItem.Location = new System.Drawing.Point(616, 33);
+            this.btnExcluirItem.Name = "btnExcluirItem";
+            this.btnExcluirItem.Size = new System.Drawing.Size(78, 29);
+            this.btnExcluirItem.TabIndex = 26;
+            this.btnExcluirItem.Text = "Excluir";
+            this.btnExcluirItem.UseVisualStyleBackColor = false;
+            // 
+            // dgvProdutos
+            // 
+            this.dgvProdutos.AllowUserToAddRows = false;
+            this.dgvProdutos.AllowUserToDeleteRows = false;
+            this.dgvProdutos.BackgroundColor = System.Drawing.Color.White;
+            this.dgvProdutos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProdutos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Prod,
+            this.Qtde});
+            this.dgvProdutos.Location = new System.Drawing.Point(13, 19);
+            this.dgvProdutos.Name = "dgvProdutos";
+            this.dgvProdutos.ReadOnly = true;
+            this.dgvProdutos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvProdutos.Size = new System.Drawing.Size(575, 148);
+            this.dgvProdutos.TabIndex = 18;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Item";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Prod
+            // 
+            this.Prod.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Prod.DataPropertyName = "Prod";
+            this.Prod.HeaderText = "Produto";
+            this.Prod.Name = "Prod";
+            this.Prod.ReadOnly = true;
+            // 
+            // Qtde
+            // 
+            this.Qtde.DataPropertyName = "Qtde";
+            this.Qtde.HeaderText = "Valor (R$)";
+            this.Qtde.Name = "Qtde";
+            this.Qtde.ReadOnly = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnPesquisarProduto);
+            this.groupBox3.Controls.Add(this.ttbProduto);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Location = new System.Drawing.Point(9, 20);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(717, 96);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Selecionar Produto";
+            // 
+            // btnPesquisarProduto
+            // 
+            this.btnPesquisarProduto.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnPesquisarProduto.Location = new System.Drawing.Point(589, 46);
+            this.btnPesquisarProduto.Name = "btnPesquisarProduto";
+            this.btnPesquisarProduto.Size = new System.Drawing.Size(105, 36);
+            this.btnPesquisarProduto.TabIndex = 24;
+            this.btnPesquisarProduto.Text = "Pesquisar Produto";
+            this.btnPesquisarProduto.UseVisualStyleBackColor = false;
+            // 
+            // ttbProduto
+            // 
+            this.ttbProduto.BackColor = System.Drawing.SystemColors.Window;
+            this.ttbProduto.Location = new System.Drawing.Point(68, 19);
+            this.ttbProduto.Name = "ttbProduto";
+            this.ttbProduto.Size = new System.Drawing.Size(626, 20);
+            this.ttbProduto.TabIndex = 25;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Produto*";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.mskTelefone);
+            this.groupBox1.Controls.Add(this.Código);
+            this.groupBox1.Controls.Add(this.btnPesquisarCliente);
+            this.groupBox1.Controls.Add(this.ttbCliente);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(21, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(742, 89);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cliente";
+            // 
+            // mskTelefone
+            // 
+            this.mskTelefone.Location = new System.Drawing.Point(77, 56);
+            this.mskTelefone.Name = "mskTelefone";
+            this.mskTelefone.Size = new System.Drawing.Size(134, 20);
+            this.mskTelefone.TabIndex = 23;
+            // 
+            // Código
+            // 
+            this.Código.AutoSize = true;
+            this.Código.Location = new System.Drawing.Point(6, 56);
+            this.Código.Name = "Código";
+            this.Código.Size = new System.Drawing.Size(52, 13);
+            this.Código.TabIndex = 20;
+            this.Código.Text = "Telefone:";
+            // 
+            // btnPesquisarCliente
+            // 
+            this.btnPesquisarCliente.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnPesquisarCliente.Location = new System.Drawing.Point(598, 44);
+            this.btnPesquisarCliente.Name = "btnPesquisarCliente";
+            this.btnPesquisarCliente.Size = new System.Drawing.Size(105, 36);
+            this.btnPesquisarCliente.TabIndex = 19;
+            this.btnPesquisarCliente.Text = "Pesquisar Cliente";
+            this.btnPesquisarCliente.UseVisualStyleBackColor = false;
+            // 
+            // ttbCliente
+            // 
+            this.ttbCliente.BackColor = System.Drawing.SystemColors.Window;
+            this.ttbCliente.Location = new System.Drawing.Point(77, 18);
+            this.ttbCliente.Name = "ttbCliente";
+            this.ttbCliente.Size = new System.Drawing.Size(626, 20);
+            this.ttbCliente.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Cliente";
+            // 
+            // btnFecharVenda
+            // 
+            this.btnFecharVenda.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnFecharVenda.Location = new System.Drawing.Point(525, 9);
+            this.btnFecharVenda.Name = "btnFecharVenda";
+            this.btnFecharVenda.Size = new System.Drawing.Size(138, 38);
+            this.btnFecharVenda.TabIndex = 13;
+            this.btnFecharVenda.Text = "FECHAR VENDA";
+            this.btnFecharVenda.UseVisualStyleBackColor = false;
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnPesquisar.Location = new System.Drawing.Point(218, 9);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(90, 38);
+            this.btnPesquisar.TabIndex = 12;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = false;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnCancelar.Location = new System.Drawing.Point(410, 9);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(90, 38);
+            this.btnCancelar.TabIndex = 10;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnSair.Location = new System.Drawing.Point(692, 9);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(71, 38);
+            this.btnSair.TabIndex = 11;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnExcluir.Location = new System.Drawing.Point(314, 9);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(90, 38);
+            this.btnExcluir.TabIndex = 9;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = false;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnAlterar.Location = new System.Drawing.Point(122, 9);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(90, 38);
+            this.btnAlterar.TabIndex = 8;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = false;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnNovo.Location = new System.Drawing.Point(26, 9);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(90, 38);
+            this.btnNovo.TabIndex = 6;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = false;
+            // 
+            // VenderProduto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Goldenrod;
+            this.ClientSize = new System.Drawing.Size(800, 497);
+            this.ControlBox = false;
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "VenderProduto";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "H.L - VENDER PRODUTO";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        public System.Windows.Forms.Label Código;
+        public System.Windows.Forms.Button btnPesquisarCliente;
+        public System.Windows.Forms.TextBox ttbCliente;
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Button btnPesquisar;
+        public System.Windows.Forms.Button btnCancelar;
+        public System.Windows.Forms.Button btnSair;
+        public System.Windows.Forms.Button btnExcluir;
+        public System.Windows.Forms.Button btnAlterar;
+        public System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.GroupBox groupBox3;
+        public System.Windows.Forms.Button btnPesquisarProduto;
+        public System.Windows.Forms.TextBox ttbProduto;
+        public System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox mskTelefone;
+        public System.Windows.Forms.Button btnFecharVenda;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        public System.Windows.Forms.Button btnExcluirItem;
+        private System.Windows.Forms.DataGridView dgvProdutos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Prod;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Qtde;
+    }
+}
